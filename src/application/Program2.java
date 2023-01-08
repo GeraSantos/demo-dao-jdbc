08b9a57052b059ecd4a53a9451aecbc05061c456
Geraldo Santos <geraldo.gsantos12@gmail.com>
2023-01-07 21:20:41 -0300

    insert and Update implementation department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.dao.SellerDao;
 import model.entities.Department;
 
 public class Program2 {
@@ -11,16 +12,26 @@ public class Program2 {
 	public static void main(String[] args) {
 		
 		DepartmentDao departmentDao = DaoFactory.creaDepartmentDao();
-		System.out.println("=== TEST 1: seller findById ===");
+		System.out.println("=== TEST 1: department findById ===");
 		Department department = departmentDao.findById(3);
 		System.out.println(department);
 		
-		System.out.println("=== TEST 2: seller findAll ===");
+		System.out.println("=== TEST 2: department findAll ===");
 		List<Department> list = departmentDao.findAll();
 		for(Department obj : list) {
 			System.out.println(obj);
 		}
-
+		/*
+		System.out.println("=== TEST 2: department insert ===");
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id: " + newDepartment.getId());
+		*/
+		System.out.println("=== TEST 2: department Update ===");
+		department = departmentDao.findById(1);
+		department.setName("Sport");
+		departmentDao.Update(department);
+		System.out.println("Update completed");
 	}
 
 }
